2a8a4794063968e744bf4899117a13c272c34180
Jessica Gonzalez <jgon91@gmail.com>
2017-06-14 20:19:32 -0700

    path to static files

--- index.js
+++ index.js
@@ -65,6 +65,7 @@ var mountPath = process.env.PARSE_MOUNT || '/parse';
 app.use(mountPath, api);
 
 app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(expressLayouts);
 app.use(cookieParser());
 app.use(methodOverride());
